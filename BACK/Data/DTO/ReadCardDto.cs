0f11365b445ac840a0d8bbb8b2fd816786628772
Fabio Rodrigo Colombini <fabiorcolombini@gmail.com>
2023-07-18 00:07:48 -0300

    Id no ReadCardDto

--- BACK/Data/DTO/ReadCardDto.cs
+++ BACK/Data/DTO/ReadCardDto.cs
@@ -4,6 +4,7 @@ namespace QuadroKanban.Data.DTO;
 
 public class ReadCardDto
 {
+	public int Id { get; set; }
     public string? Titulo { get; set; }
     public string? conteudo { get; set; }
     public string? lista { get; set; }
